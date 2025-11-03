946b59c87a10ee2b256a0b200ff597da86eeaf75
Imasha <imashaudayanganie2020@gmail.com>
2025-11-04 02:01:55 +0530

    updated

--- To-Do-Frontend/to-do-app/src/pages/AdminDashboard.jsx
+++ To-Do-Frontend/to-do-app/src/pages/AdminDashboard.jsx
@@ -5,7 +5,7 @@ function AdminDashboard() {
       <div className="min-h-screen bg-gradient-to-br from-slate-900 via-slate-800 to-slate-700 flex items-center justify-center text-white">
       <div className="bg-white/10 backdrop-blur-xl rounded-2xl p-10 shadow-2xl max-w-xl">
         <h1 className="text-4xl font-bold mb-4 text-center">ADMIN DASHBOARD</h1>
-        <p className="text-center text-gray-300">Welcome Admin. Manage the system here.</p>
+        <p className="text-center text-gray-300">Welcome Admin.. Manage the system here.</p>
       </div>
     </div>
   )
